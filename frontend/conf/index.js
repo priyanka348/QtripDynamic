bba6924a32a630b2ec0bc36fca30d0b669cdc770
priyanka348 <80335143+priyanka348@users.noreply.github.com>
2022-11-24 17:43:29 +0530

    Add files via upload

---
+++ frontend/conf/index.js
@@ -0,0 +1,4 @@
+
+const config = { backendEndpoint: "http://3.111.235.236:8082" };
+
+export default config;
---
+++ frontend/modules/adventures_page.js
@@ -0,0 +1,175 @@
+
+import config from "../conf/index.js";
+
+//Implementation to extract city from query params
+function getCityFromURL(search) {
+  // TODO: MODULE_ADVENTURES
+  // 1. Extract the city id from the URL's Query Param and return it
+  let params = new URLSearchParams(search);
+  let city = params.get('city');
+  return city;
+
+}
+
+//Implementation of fetch call with a paramterized input based on city
+async function fetchAdventures(city) {
+  // TODO: MODULE_ADVENTURES
+  // 1. Fetch adventures using the Backend API and return the data
+  try{
+  let response = await fetch(config.backendEndpoint + `/adventures?city=${city}`);
+ 
+  const data = await response.json();
+  return data;
+ 
+  }catch(err){
+    return null;
+  }
+
+}
+
+//Implementation of DOM manipulation to add adventures for the given city from list of adventures
+function addAdventureToDOM(adventures) {
+  // TODO: MODULE_ADVENTURES
+  // 1. Populate the Adventure Cards and insert those details into the DOM
+  let dataElem = document.getElementById("data");
+  adventures.forEach(obj => {
+    let {id,name,costPerHead,currency,image,duration,category} = obj;
+   dataElem.innerHTML += `
+   <div class="col-6 col-lg-3 mb-4 position-relative">
+   <a href="detail/?adventure=${id}" id=${id}>
+   <div class="category-banner">${category}</div>
+     <div class="card activity-card"> 
+        <img src="${image}" />
+        <div class="card-body col-md-12 mt-12"> 
+         
+            <div class="d-flex justify-content-between flex-wrap">
+            <p>${name}</p>
+            <p>₹${costPerHead}</p>
+            </div>
+
+            <div class="d-flex justify-content-between">
+            <p>Duration</p>
+            <p>${duration} Hours</p>
+            </div>
+            
+        </div>  
+      </div>
+    
+   </a>
+ </div>
+   `
+  })
+
+}
+
+//Implementation of filtering by duration which takes in a list of adventures, the lower bound and upper bound of duration and returns a filtered list of adventures.
+function filterByDuration(list, low, high) {
+  // TODO: MODULE_FILTERS
+  // 1. Filter adventures based on Duration and return filtered list
+ let filteredList = list.filter(key => key.duration >= low && key.duration <= high);
+  return filteredList;
+}
+
+//Implementation of filtering by category which takes in a list of adventures, list of categories to be filtered upon and returns a filtered list of adventures.
+function filterByCategory(list, categoryList) {
+  // TODO: MODULE_FILTERS
+  // 1. Filter adventures based on their Category and return filtered list
+   let filteredList = [];
+   categoryList.forEach((category) => {
+     list.forEach((key) => {
+       if(key.category === category){
+          filteredList.push(key);
+       }
+     });
+   });
+  // let filteredList = list.filter(adventure => categoryList.includes(adventure.category));
+   return filteredList;
+}
+
+// filters object looks like this filters = { duration: "", category: [] };
+
+//Implementation of combined filter function that covers the following cases :
+// 1. Filter by duration only
+// 2. Filter by category only
+// 3. Filter by duration and category together
+
+function filterFunction(list, filters) {
+  // TODO: MODULE_FILTERS
+  // 1. Handle the 3 cases detailed in the comments above and return the filtered list of adventures
+  // 2. Depending on which filters are needed, invoke the filterByDuration() and/or filterByCategory() methods
+  let filteredList = [];
+ 
+  if(filters["duration"].length > 0 && filters["category"].length > 0){
+    let choice = filters["duration"].split("-");
+    filteredList = filterByDuration(
+      list,
+      parseInt(choice[0]),
+      parseInt(choice[1])
+      );
+    filteredList = filterByCategory(filteredList,filters["category"]);
+  }
+  else if(filters["duration"].length > 0){
+    let choice = filters["duration"].split("-");
+    filteredList = filterByDuration(list,parseInt(choice[0]),parseInt(choice[1]));
+  }
+  
+  else if(filters["category"].length > 0) {
+    filteredList = filterByCategory(list,filters["category"]);
+  }
+  
+ else{
+  filteredList = list;
+ }
+  // Place holder for functionality to work in the Stubs
+  return filteredList;
+}
+
+//Implementation of localStorage API to save filters to local storage. This should get called everytime an onChange() happens in either of filter dropdowns
+function saveFiltersToLocalStorage(filters) {
+  // TODO: MODULE_FILTERS
+  // 1. Store the filters as a String to localStorage
+  localStorage.setItem("filters", JSON.stringify(filters));
+  return true;
+}
+
+//Implementation of localStorage API to get filters from local storage. This should get called whenever the DOM is loaded.
+function getFiltersFromLocalStorage() {
+  // TODO: MODULE_FILTERS
+  // 1. Get the filters from localStorage and return String read as an object
+  return JSON.parse(localStorage.getItem("filters"));
+
+  // Place holder for functionality to work in the Stubs
+
+}
+
+//Implementation of DOM manipulation to add the following filters to DOM :
+// 1. Update duration filter with correct value
+// 2. Update the category pills on the DOM
+
+function generateFilterPillsAndUpdateDOM(filters) {
+  // TODO: MODULE_FILTERS
+  // 1. Use the filters given as input, update the Duration Filter value and Generate Category Pills
+  document.getElementById("duration-select").value = filters.duration;
+  filters["category"].forEach((key) => {
+    // let ele = document.createElement("div");
+    // ele.className = "category-filter";
+    // ele.innerHTML = `<div>${key}</div>`;
+    let pEle = document.getElementById("category-list");
+    let ele = document.createElement("div");
+    ele.className = "category-filter";
+    ele.innerHTML = key;
+    pEle.appendChild(ele);
+  });
+
+}
+export {
+  getCityFromURL,
+  fetchAdventures,
+  addAdventureToDOM,
+  filterByDuration,
+  filterByCategory,
+  filterFunction,
+  saveFiltersToLocalStorage,
+  getFiltersFromLocalStorage,
+  generateFilterPillsAndUpdateDOM,
+};
